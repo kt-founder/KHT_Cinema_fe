17b47d926c896685f359f403e2cb2265a0e1c8f8
Your Name <you@example.com>
2024-10-17 22:34:41 +0700

    Update logo, Nav, header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,30 @@
 // src/components/Navbar.js
 import React from 'react';
 import { Menu } from 'antd';
-
+import "../pages/styles/global.css"
+import logo from "../assets/images/logo2.png"
 const Navbar = () => {
-  return (
-    <Menu mode="horizontal" style={{ backgroundColor: '#0000FF', color: '#0000FF', justifyContent: 'center', padding: '10px 5' }}>
-      <Menu.Item key="home" style={{ color: '#fff' }}>Trang chủ</Menu.Item>
-      <Menu.Item key="schedule" style={{ color: '#fff' }}>Lịch chiếu</Menu.Item>
-      <Menu.Item key="promo" style={{ color: '#fff' }}>Khuyến mãi</Menu.Item>
-      <Menu.Item key="price" style={{ color: '#fff' }}>Giá vé</Menu.Item>
-      <Menu.Item key="festival" style={{ color: '#fff' }}>Liên hoan phim</Menu.Item>
-      <Menu.Item key="about" style={{ color: '#fff' }}>Giới thiệu</Menu.Item>
-      <Menu.Item key="SignUp" style={{ color: '#fff' }}> <a href="/signup">Đăng kí</a></Menu.Item>
-      <Menu.Item key="Login" href="/login" style={{ color: '#fff' }}> <a href="/login">Đăng nhập</a></Menu.Item>
-    </Menu>
-  );
+    return (
+        <Menu mode="horizontal" className="navbar" style={{ justifyContent: 'start' }}>
+            <Menu.Item key="logo" style={{ padding: '0 20px' }}>
+                <a href="/">
+                    <img
+                        src={logo}  // Đường dẫn đến logo
+                        alt="Logo"
+                        style={{ height: '75px', marginRight: '70px' }} // Điều chỉnh kích thước logo
+                    />
+                </a>
+            </Menu.Item>
+            <Menu.Item key="home">Trang chủ</Menu.Item>
+            <Menu.Item key="schedule">Lịch chiếu</Menu.Item>
+            <Menu.Item key="promo">Khuyến mãi</Menu.Item>
+            <Menu.Item key="price">Giá vé</Menu.Item>
+            <Menu.Item key="festival">Liên hoan phim</Menu.Item>
+            <Menu.Item key="about">Giới thiệu</Menu.Item>
+            <Menu.Item key="SignUp"> <a href="/signup">Đăng kí</a></Menu.Item>
+            <Menu.Item key="Login"> <a href="/login">Đăng nhập</a></Menu.Item>
+        </Menu>
+    );
 };
 
 export default Navbar;
