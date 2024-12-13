8ef3e70b718012905b511142f92232268803e1dc
NgocHieuGithub <langochieud20ptit@gmail.com>
2024-12-13 14:38:00 +0700

    Revise layout

--- src/pages/User/Payment-Success.js
+++ src/pages/User/Payment-Success.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 function PaymentSuccess() {
     return (
-        <div>
+        <div  style={{textAlign:'center'}}>
             <h1 style={{color:'black'}}>Thanh toán thành công!</h1>
             <button onClick={() => window.location.href = "/"}>Quay về trang chủ</button>
         </div>
